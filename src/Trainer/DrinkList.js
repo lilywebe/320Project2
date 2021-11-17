88111e8d64784e4c91bcd55d0f1778ae9c382557
lilywebe <55253338+lilywebe@users.noreply.github.com>
2021-11-17 09:48:49 -0500

    adding inital files and drinks displayed on screen

---
+++ src/Trainer/DrinkList.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useRef } from "react";
+
+export default function DrinkList(props) {
+  let drinksEls = props.drinks.map((drink) => (
+    <div
+      key={drink.id}
+      //className={props.curTraining.id == drink.id ? "active" : ""}
+      onClick={() => {
+        props.onSelected(drink.id);
+      }}
+    >
+      {drink.name}
+    </div>
+  ));
+
+  return <div>{drinksEls}</div>;
+}
---
+++ src/Trainer/Trainer.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useRef } from "react";
+import DrinkList from "./DrinkList";
+import DrinkVis from "./DrinkVis";
+export default function Trainer() {
+  const [drinks, setDrinks] = useState([]);
+  const [curTraining, setCurTraining] = useState({});
+
+  useEffect(() => {
+    fetch("data/drinks.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setDrinks(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      Barista Trainer
+      <div>
+        Now Learning: {curTraining.name}
+      </div>
+      <DrinkList
+        drinks={drinks}
+        curTraining={curTraining}
+        onSelected={(id) => {
+          drinkSelected(id);
+        }}
+      />
+    </div>
+  );
+
+  function drinkSelected(id) {
+    //find that drink
+    const foundDrink = drinks.find((drink) => drink.id == id);
+    //set it as currently learning
+    setCurTraining(foundDrink);
+  }
+}
