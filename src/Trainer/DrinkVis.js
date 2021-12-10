86a6bbc638c524c611d92be6afb7e5dd724d6e4d
lilywebe <55253338+lilywebe@users.noreply.github.com>
2021-12-10 14:35:19 -0500

    finally figured out svg animations

--- src/Trainer/DrinkVis.js
+++ src/Trainer/DrinkVis.js
@@ -28,6 +28,11 @@ export default function DrinkVis(props) {
   };
   //end mui doc section
 
+  //get the new class when button clicked
+function getClass(index){
+  var cn = temp[props.curTraining.id][index] ? "clicked" : "";
+  return cn;
+}
 
   if (props.drinks.length > 0) {
     var temp = clicked;
@@ -44,10 +49,11 @@ export default function DrinkVis(props) {
 
     //loop through positions for cur drink and map circles to those positions on the screen
     for (const [index, value] of props.curTraining.positions.entries()) {
-      console.log(temp);
-      circles.push(<circle className={temp[props.curTraining.id][index] ? "clicked" : "unclicked"} r="10" cx={props.curTraining["positions"][index].x} cy={props.curTraining["positions"][index].y} fill="#FFFFC9" stroke="black" strokeWidth="1" />)
+      circles.push(<circle class= {getClass(index)} r="10" cx={value.x} cy={value.y} fill="#FFFFC9" stroke="black" strokeWidth="1" />)
+      //add text on top of circles
+      text.push(<text class={getClass(index)} x={value.x - 5} y={value.y + 5} fill="#FFFFC9" stroke="black" strokeWidth="1">{index + 1}</text>);
       //add clear circle on top of circle and text to make text clickable as well
-      clickableArea.push(<circle r="10" cx={props.curTraining["positions"][index].x} cy={props.curTraining["positions"][index].y} opacity="0" onClick={() => {
+      clickableArea.push(<circle  r="10" cx={value.x} cy={value.y} opacity="0" onClick={() => {
         //if the circle has already been clicked, do nothing
         if (temp[props.curTraining.id][index] == true) {
 
@@ -61,14 +67,10 @@ export default function DrinkVis(props) {
           setShowModal(true);
           setClickArray(props.curTraining.id, index, clicked, setClicked);
         }
-      }} />)
+      }}/>)
     };
 
-    //add text on top of the SVG circles from the last loop
-    for (const [index, value] of props.curTraining.positions.entries()) {
-      text.push(<text x={props.curTraining["positions"][index].x - 5} y={props.curTraining["positions"][index].y + 5} fill="#FFFFC9" stroke="black" strokeWidth="1">{index + 1}</text>
-      )
-    };
+    
 
     return (
       //show image, circles, and text that correlate to the current drink
